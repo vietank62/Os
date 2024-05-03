f6a8394a775bd6c423677277d88d44855df86277
vietank62 <anh.phamviet241103@hcmut.edu.vn>
2024-05-04 06:19:36 +0700

    fisrt project

--- src/queue.c
+++ src/queue.c
@@ -2,19 +2,43 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t * q) {
-        if (q == NULL) return 1;
-	return (q->size == 0);
+int empty(struct queue_t *q)
+{
+        if (q == NULL)
+                return 1;
+        return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
         /* TODO: put a new process to queue [q] */
+        if (q == NULL || proc == NULL)
+        {
+                return;
+        }
+        if (q->size >= MAX_QUEUE_SIZE)
+        {
+                printf("Queue is full.\n");
+                return;
+        }
+        q->proc[q->size] = proc;
+        q->size++;
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
+struct pcb_t *dequeue(struct queue_t *q)
+{
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+        if (q == NULL || q->size == 0)
+        {
+                return NULL;
+        }
+        struct pcb_t *first_proc = q->proc[0];
+        for (int i = 0; i < q->size - 1; i++)
+        {
+                q->proc[i] = q->proc[i + 1];
+        }
+        q->size--;
+        return first_proc;
 }
-
